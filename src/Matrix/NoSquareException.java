75b9568313e843de4cbed78f64870816004db851
Suvimal Yashraj <suvi.yashraj@gmail.com>
2016-04-14 18:52:58 +0530

    First Commit

---
+++ src/Matrix/NoSquareException.java
@@ -0,0 +1,27 @@
+package Matrix;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MAHE
+ */
+public class NoSquareException extends Exception {
+
+	public NoSquareException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSquareException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+
